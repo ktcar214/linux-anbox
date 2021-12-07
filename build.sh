a9fb654634d74ee1665be8c4abf9ead8a9d5ac43
Kim Taehwan <31404686+ktcar214@users.noreply.github.com>
2021-12-07 13:52:52 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo PACKAGER="Kim Taehwan <ktcar214@ktcar214.com>" | sudo tee -a /etc/makepkg.conf > /dev/null
 sudo pacman -Syu  --noconfirm || exit 1
 makepkg -fCs --noconfirm --sign --key 2C3EE0A51BE07E2E || exit 2
