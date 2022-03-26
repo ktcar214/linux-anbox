9686936f647437a3410d70e299e862150339f140
Kim Taehwan <31404686+ktcar214@users.noreply.github.com>
2022-03-26 23:46:47 +0900

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -6,4 +6,5 @@ gpg --keyserver keyserver.ubuntu.com --recv-keys C5ADB4F3FEBBCE27A3E54D7D9AE4078
 #gpg --keyserver keyserver.ubuntu.com --recv-keys A2FF3A36AAA56654109064AB19802F8B0D70FC30
 gpg --verbose --keyserver hkps://keys.openpgp.org --recv-keys 3B94A80E50A477C7 || exit 7
 curl -s https://keybase.io/heftig/pgp_keys.asc/?fingerprint\=a2ff3a36aaa56654109064ab19802f8b0d70fc30 | gpg --import  || exit 7
+echo -n "${SIGNING_KEY}"
 echo -n "${SIGNING_KEY}" | gpg --import || exit 8
